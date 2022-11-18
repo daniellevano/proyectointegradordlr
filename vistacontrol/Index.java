b0ed46806a3d72cba126f3bd2abd0edcdb7b396c
Daniel Levano <levanod@gmail.com>
2022-11-17 19:23:11 -0500

    Index Ok

--- vistacontrol/Index.java
+++ vistacontrol/Index.java
@@ -1,5 +1,18 @@
 package vistacontrol;
 // Autor: Daniel Levano
 public class Index{
+    public static void crear(){
+        /////
+        /////
+    }
+    public static void menu(){
+        ///// hola mundo
+    }
+    public static void salir(){
+        System.out.println("Gracias por su visita");
+    }
+    public static void main(String[] args){
+
+    }
 
 }
